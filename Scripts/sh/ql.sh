5d4c779af4b59ea1edb10f395d2ff2b006f75ffd
3382438 <74660415+3382438@users.noreply.github.com>
2022-04-12 12:47:00 +0800

    Update ql.sh

--- Scripts/sh/ql.sh
+++ Scripts/sh/ql.sh
@@ -14,7 +14,7 @@ echo -e "\e[36m
        ▀                        ▀████▀▀                                 ▀████▀▀
 \e[0m\n"
 
-DOCKER_IMG_NAME="whyour/qinglong:2.10.6"
+DOCKER_IMG_NAME="whyour/qinglong:2.10.9"
 QL_PATH=""
 SHELL_FOLDER=$(pwd)
 CONTAINER_NAME=""
